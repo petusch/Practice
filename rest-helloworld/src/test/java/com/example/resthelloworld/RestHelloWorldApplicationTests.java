36e841e58c6b94495e50c1b1bbce93f8f0ded58a
Oleg <34604253+petusch@users.noreply.github.com>
2020-02-25 18:58:24 +0300

    First commit
    Restful hello world, based on http get request, which returns object, automaticlly converted to json file.

---
+++ rest-helloworld/src/test/java/com/example/resthelloworld/RestHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resthelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
